a2e618a86d5e4d93456c66accf9b92acd638ee19
Chong Ren <leechongren@users.noreply.github.com>
2020-01-24 12:34:33 +0800

    added pokemon movesets from separate json file

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -5,11 +5,12 @@ import PokemonObj from '../pokemon/pokemondetails'
 const PokemonDetails = (props) => {
     const uniqueId = props.match.params.id
     const pokemonName = PokemonData[uniqueId - 1].name.english
+    const pokemonMoves = Object.values(PokemonObj)[uniqueId - 1].moves.join(",")
     return <div>
         <img className="pokemon-card__image" alt="Not Loaded" src={process.env.PUBLIC_URL + `pokemonImage/${uniqueId}.png`} />
         <div>
-            Name: {pokemonName}
-
+            Name: {pokemonName} <br />
+            Moves: {pokemonMoves}
         </div>
     </div>
 }
